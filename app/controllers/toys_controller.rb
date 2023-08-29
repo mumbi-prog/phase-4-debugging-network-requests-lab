f3519bdcefec3ee5f662a750a411683affdd5788
Sylvia Mumbi <mumbi099@gmail.com>
2023-08-29 14:17:14 +0300

    Done lab

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,5 @@
 class ToysController < ApplicationController
+
   wrap_parameters format: []
 
   def index
@@ -7,13 +8,16 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :ok
   end
 
   def destroy
